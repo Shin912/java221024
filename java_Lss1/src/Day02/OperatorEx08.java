cd01783f689c72f6a3d072d68f7af7b83f06e8b1
shin129 <lss1994@naver.com>
2022-10-25 21:44:06 +0900

    java day02 : 연산자 짝수홀수

---
+++ java_Lss1/src/Day02/OperatorEx08.java
@@ -0,0 +1,23 @@
+package Day02;
+
+import java.util.Scanner;
+
+public class OperatorEx08 {
+
+	public static void main(String[] args) {
+		/* 정수를 입력받아 입력받은 정수가 홀수인지 짝수인지 출력하는 코드를 작성
+		 * 예시
+		 * 정수를 입력하세요 : 5
+		 * 5는 홀수입니다.
+		 * */
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("정수를 입력하세요 : ");
+		int num = scan.nextInt();
+		String result = num % 2 == 0 ? "짝수" : "홀수";
+		System.out.println(num + "은(는) " + result + "이다. ");
+		
+		scan.close();
+	}
+
+}
