1a403317480e2c5a536f6e49529055a8ce83c261
shin129 <lss1994@naver.com>
2022-10-26 21:46:07 +0900

    java day03 : 반복문 ( for ) 구구단

---
+++ java_Lss1/src/Day03/ForEx03.java
@@ -0,0 +1,16 @@
+package Day03;
+
+public class ForEx03 {
+
+	public static void main(String[] args) {
+		//구구단 2단을 출력하는 코드를 작성하세요.
+		int i = 1;
+		int num = 3;
+		
+		for (i = 1; i <= 9; i++) {
+			System.out.println(num + " x " + i + " = " + (num*i));
+		}
+
+	}
+
+}
