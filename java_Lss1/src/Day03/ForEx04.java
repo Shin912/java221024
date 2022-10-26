67c305dcd06283ad816609889ad2ff80a9dbf1cb
shin129 <lss1994@naver.com>
2022-10-26 21:46:34 +0900

    java day03 : 반복문 ( for ) 1~10 짝수 출력

---
+++ java_Lss1/src/Day03/ForEx04.java
@@ -0,0 +1,45 @@
+package Day03;
+
+public class ForEx04 {
+
+	public static void main(String[] args) {
+		/* 1부터 10사이의 짝수를 출력하는 코드를 작성
+		 * 
+		 * 방법1
+		 * 반복횟수 : i는 1부터 5까지 1씩증가
+		 * 규칙성 : 2*i 를 출력
+		 */
+		int i = 1;		
+		for ( i = 1; i <= 5; i++ ) {
+			System.out.print( i*2 + " ");
+		}
+		System.out.println();
+		
+		
+		 /* 방법2
+		 * 반복횟수 : i는 2부터 10까지 2씩 증가
+		 * 규칙성 : i를 출력
+		 */ 
+		for( i = 2; 2*i <= 5 ; i++) {
+			System.out.print( i + " " );
+		}
+		System.out.println();
+		
+		for( i = 2; i <= 10; i += 2) {
+			System.out.print( i + " ");
+		}
+		System.out.println();
+
+		 /* 방법3
+		 * 반복횟수 : i는 1부터 10까지 1씩 증가
+		 * 규칙성 i가 짝수이면 i를 출력
+		 * */
+		for( i = 1; i <= 10; i += 1) {
+			if(i % 2 == 0) {
+				System.out.print(i + " ");
+			}
+		}
+
+	}
+
+}
