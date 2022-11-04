97ff3cb350d2c3afc6b940af5078d8184289b58e
shin129 <lss1994@naver.com>
2022-11-04 17:42:47 +0900

    java day10 : Mathod Record

---
+++ java_Lss1/src/Day10/Record.java
@@ -0,0 +1,18 @@
+package Day10;
+
+public class Record {
+	private int count;
+	private String name;
+	
+	public Record(int count, String name) {
+		this.count = count;
+		this.name = name;
+	}
+	
+	public void print() {
+		System.out.println(name + " : " + count);
+	}
+	public int getCount() {
+		return count;
+	}
+}
