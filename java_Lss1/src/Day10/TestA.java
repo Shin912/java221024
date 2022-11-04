389a1d104828b0a8b1f84a3427b3f08a8e63ca25
shin129 <lss1994@naver.com>
2022-11-04 21:18:48 +0900

    java day10 : 접근제어자  예제

---
+++ java_Lss1/src/Day10/TestA.java
@@ -0,0 +1,12 @@
+package Day10;
+
+public class TestA {
+
+
+	private int num1;
+	int num2;
+	public int num3;
+
+	
+
+}
---
+++ java_Lss1/src/example_student/TestB.java
@@ -0,0 +1,12 @@
+package example_student;
+
+public class TestB {
+
+
+	private int num1;
+	int num2;
+	public int num3;
+
+	
+
+}
