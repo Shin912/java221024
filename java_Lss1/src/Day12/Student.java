84a301e622df7753e8afdecda06eb64f65af39da
shin129 <lss1994@naver.com>
2022-11-08 21:47:05 +0900

    java day12 : 오브젝트 예제

---
+++ java_Lss1/src/Day12/Student.java
@@ -0,0 +1,43 @@
+package Day12;
+
+public class Student {
+
+	public int grade, clssNum, num;
+	public String name;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + clssNum;
+		result = prime * result + grade;
+		result = prime * result + num;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		//같은 객체 공유
+		if (this == obj)
+			return true;
+		//null과 비교 불가능
+		if (obj == null)
+			return false;
+		//obj가 같은 클래스의 객체가 아닌 경우
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		if (clssNum != other.clssNum)
+			return false;
+		if (grade != other.grade)
+			return false;
+		if (num != other.num)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Student [grade=" + grade + ", clssNum=" + clssNum + ", num=" + num + ", name=" + name + "]";
+	}
+		
+
+
+}
