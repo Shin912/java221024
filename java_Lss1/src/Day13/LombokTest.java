908b53e946ae2a293a5d8aaa4f04df755bbbf807
shin129 <lss1994@naver.com>
2022-11-09 21:07:59 +0900

    java day13: lombok 예제

---
+++ java_Lss1/src/Day13/LombokTest.java
@@ -0,0 +1,21 @@
+package Day13;
+
+import lombok.Data;
+
+public class LombokTest {
+
+	public static void main(String[] args) {
+		TestA a = new TestA();
+		a.setNum(10);
+		System.out.println(a.getNum());
+		System.out.println(a); // toString() 확인
+		TestA b = new TestA();
+		b.setNum(10);
+		System.out.println(a.equals(b)); //equals() 확인
+	}
+
+}
+@Data //@Getter, @Setter, @ToString, @EqualsAndHashCode, @RequiredArgsConstrucor
+class TestA{
+	private int num;
+}
