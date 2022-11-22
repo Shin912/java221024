af023c6d6f4cf1a6b660027bfae3778b5be32e7a
shin129 <lss1994@naver.com>
2022-11-22 21:34:55 +0900

    java day22 : 학생 출석부 관리 (강사님)

---
+++ java_Lss1/src/Day22/AttendanceMain_02.java
@@ -0,0 +1,200 @@
+package Day22;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class AttendanceMain_02 {
+	
+	static Scanner scan = new Scanner(System.in);
+	/* 출석부 관리 프로그램
+	 * 1. 학생 등록
+	 * 		- 이름과 생년월일
+	 * 3. 학생 수정
+	 * 		- 이름과 생년월일로 검색해서 이름과 생년월일 수정
+	 * 3. 학생 삭제
+	 * - 이름과 생년월일로 검색해서 삭제
+	 * 4. 학생 출결 확인
+	 * 	- 이름과 생년월일로 검색해서 해당 학생의 모든 출결을 확인
+	 * 	- 2022-11-22 : O
+	 * 5. 출석 확인 
+	 * 		-날짜 입력 : 2022-11-22
+	 * 		-(출석 : O, 지각 : / , 조퇴 : \, 결석 : X
+	 * 	홍길동 출석현황 : X
+	 * 	고길동 출석현황 : O
+	 * 	임꺽정 출석현황 : /
+	 * 6. 출석 수정
+	 * 		- 날짜 입력
+	 * 		- 수정할 학생의 이름과 생년월일 입력
+	 * 7. 출석 삭제
+	 * 	- 날짜 입력하여 해당 날짜 출석 삭제
+	 * 8. 날짜별 출석 확인
+	 * 	- 날짜 입력하여 해당 날짜의 모든 학생의 출석 여부를 확인
+	 * 9.프로그램 종료
+	 * */
+	
+	public static void main(String[] args) {
+		
+		int menu = -1;
+		Attendance attendance = new Attendance();
+		do {
+			printMenu();
+			
+			menu = scan.nextInt();
+			scan.nextLine();
+			System.out.println("======================");
+			runMenu(menu, attendance);
+			
+		}while(menu != 3);
+	}
+
+	private static void runMenu(int menu, Attendance attendance) {
+		ArrayList<Student> stds = attendance.getStd();
+		ArrayList<Log> logs = attendance.getLogs();
+		switch(menu) {
+		case 1:
+			managermentStduents(stds);
+			
+			break;
+		case 2:
+			managermentLogs(logs, stds);
+			break;
+		case 3:
+			printStr("프로그램 종료..!");
+			break;
+		default:
+			printStr("잘못된 메뉴 선택입니다.");
+		}
+		
+	}
+	
+	private static void managermentLogs(ArrayList<Log> logs, ArrayList<Student> stds) {
+		
+		
+	}
+
+	private static void managermentStduents(ArrayList<Student> stds) {
+		printSubMenu(1);
+		int subMenu = scan.nextInt();
+		scan.nextLine();
+		printBar();
+		switch(subMenu) {
+		case 1:		addStrudent(stds);	break;
+		case 2:		updateStudent(stds); break;
+		case 3:		deleteStudent(stds); break;
+		case 4:
+			printStr("취소 했습니다");
+			break;
+		default:
+			printStr("메뉴를 잘못입력했습니다");
+		}
+		
+	}
+
+	private static void deleteStudent(ArrayList<Student> stds) {
+		if(stds == null) {
+			throw new RuntimeException("예외 발생 : 학생 정보를 관리하는 리스트가 없습니다.");
+		}
+		System.out.print("이름 입력 : ");
+		String name = scan.nextLine();
+		System.out.print("생년월일 입력 (예시 : 940912) : ");
+		String birth = scan.nextLine();
+		Student std = new Student(name, birth);
+		int index = stds.indexOf(std); // 번지 확인
+		if( index == -1) {
+			printStr("입력 정보와 일치하는 학생이 없습니다.");
+			return;
+		}
+		stds.remove(index);
+		printStr("학생 정보를 삭제했습니다.");
+		
+	}
+
+	private static void updateStudent(ArrayList<Student> stds) {
+		if(stds == null) {
+			throw new RuntimeException("예외 발생 : 학생 정보를 관리하는 리스트가 없습니다.");
+		}
+		System.out.print("이름 입력 : ");
+		String name = scan.nextLine();
+		System.out.print("생년월일 입력 (예시 : 940912) : ");
+		String birth = scan.nextLine();
+		Student std = new Student(name, birth);
+		int index = stds.indexOf(std); // 번지 확인
+		if( index == -1) {
+			printStr("입력 정보와 일치하는 학생이 없습니다.");
+			return;
+		}
+		System.out.print("수정할 이름 입력 : ");
+		name = scan.nextLine();
+		System.out.print("수정할 생년월일 입력 (예시 : 940912) : ");
+		birth = scan.nextLine();
+		//Student serchStd = stds.get(index); // 찾은 학생정보를 가져옴
+		std = new Student(name, birth);
+		stds.set(index, std); // 전체를 바꾸기 때문에 set사용
+		printStr("학생 정보를 수정했습니다.");
+		System.out.println(stds); //확인용
+		
+	}
+
+	private static void addStrudent(ArrayList<Student> stds) {
+		if(stds == null) {
+			throw new RuntimeException("예외 발생 : 학생 정보를 관리하는 리스트가 없습니다.");
+		}
+		System.out.print("이름 입력 : ");
+		String name = scan.nextLine();
+		System.out.print("생년월일 입력 (예시 : 940912) : ");
+		String birth = scan.nextLine();
+		Student std = new Student(name, birth);
+		if(stds.indexOf(std) != -1) {
+			printStr("학생 정보를 추가하지 못했습니다");
+			return;
+		}
+		stds.add(std);
+		printStr("학생 정보를 추가했습니다");
+	}
+
+
+	private static void printBar() {
+		System.out.println("===================");
+	}
+	private static void printStr(String str) {
+		System.out.println(str);
+		printBar();
+	}
+
+	private static void printMenu() {
+		System.out.println("======= 메 뉴 ======");
+		System.out.println("1. 학생 관리");
+		System.out.println("2. 출석 관리");
+		System.out.println("3. 프로그램 종료");
+		System.out.println("===================");
+		System.out.print("메뉴 선택 : ");
+	}
+	private static void printSubMenu(int menu) {
+		switch(menu) {
+		case 1:
+			System.out.println("===== 학 생 관 리 메 뉴 ====");
+			System.out.println("1. 학생 등록");
+			System.out.println("2. 학생 수정");
+			System.out.println("3. 학생 삭제");
+			System.out.println("4. 취소");
+			System.out.println("======================");
+			System.out.print("메뉴 선택 : ");
+			break;
+		case 2:
+			System.out.println("===== 출 석 관 리 메 뉴 ====");
+			System.out.println("1. 출석 체크 - 전체");
+			System.out.println("2. 출석 확인 - 개별");
+			System.out.println("3. 출석 확인 - 날짜별");
+			System.out.println("4. 출석 수정");
+			System.out.println("5. 출석 삭제");
+			System.out.println("6. 취소");
+			System.out.println("======================");
+			System.out.print("메뉴 선택 : ");
+			break;
+		default:
+			printStr("잘못된 메뉴입니다.");
+			
+		}
+	}
+}
