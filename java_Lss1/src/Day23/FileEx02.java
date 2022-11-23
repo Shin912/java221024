edea93e8159c65dab36fd167c4e30c58a3b61fdd
shin129 <lss1994@naver.com>
2022-11-23 21:27:25 +0900

    java day 23 : 폴더 생성 예제

---
+++ java_Lss1/src/Day23/FileEx02.java
@@ -0,0 +1,40 @@
+package Day23;
+
+import java.io.File;
+import java.io.IOException;
+
+public class FileEx02 {
+
+	public static void main(String[] args) {
+		File f= new File("D:/test2/test"); // '/','\\' 둘중 하나 사용 가능
+		
+			if(f.mkdir()) {
+				System.out.println("폴더가 생성되었습니다.");
+			}else {
+				System.out.println("폴더 생성에 실패했습니다.");
+				//mkdir()가 false 인 경우
+				//1. 이미 있는 폴더인 경우
+				//2. 상위 폴더가 없는 경우
+			}
+			
+			if(f.mkdirs()) {
+				// mkdirs() = 경로에 폴더 전부 생성
+				System.out.println("폴더들 생성되었습니다.");
+			}else {
+				System.out.println("이미 폴더들이 있습니다.");
+				
+			}
+			if(f.isDirectory())
+				System.out.println("폴더명 : " + f.getName());
+			if(f.isFile())
+				System.out.println("파일명 : " + f.getName());
+			
+			if(f.delete()) {
+				System.out.println("폴더가 삭제되었습니다.");
+			}else {
+				System.out.println("없는 폴더 입니다.");
+			}
+		
+	}
+
+}
