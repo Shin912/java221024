2fa30a473565ec2e0265a8324650275bacd7abd4
shin129 <lss1994@naver.com>
2022-12-27 21:49:00 +0900

    DB day 15 대학교 관리 프로그램

---
+++ java_Lss1/src/db/day03/DBConnector.java
@@ -0,0 +1,37 @@
+package db.day03;
+
+import java.sql.*;
+
+public class DBConnector {
+	
+	public Connection con;
+	public ResultSet rs;
+	public Statement stmt;
+	public PreparedStatement pstmt;
+	
+	public DBConnector(String url, String id, String pw) {
+		try {
+			con = DriverManager.getConnection(url, id, pw);
+			stmt = con.createStatement();
+		} catch (SQLException e) {
+			System.out.println("DB 연동 실패");
+			e.printStackTrace();
+		}
+	}
+	
+	public void close() {
+		try {
+			if(con != null && !con.isClosed())
+				con.close();
+			if(stmt != null && !stmt.isClosed())
+				stmt.close();
+			if(pstmt != null && !pstmt.isClosed())
+				pstmt.close();
+			if(rs != null && !rs.isClosed())
+				rs.close();
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
