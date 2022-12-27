b03e217704c102db7a2275c038d8d7edf593e008
shin129 <lss1994@naver.com>
2022-12-27 21:48:07 +0900

    DB day 15 : 대학교 관리 프로그램

---
+++ java_Lss1/src/db/day03/UniversityProgramMain.java
@@ -0,0 +1,10 @@
+package db.day03;
+
+public class UniversityProgramMain {
+
+	public static void main(String[] args) {
+		UniversityController uc = new UniversityController();
+		uc.run();
+	}
+
+}
