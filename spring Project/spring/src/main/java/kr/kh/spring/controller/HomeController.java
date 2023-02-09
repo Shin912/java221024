994bf7c25d262080c7ea8746acb74ff9abd3692e
shin129 <lss1994@naver.com>
2023-02-09 21:14:19 +0900

    스프링 2일차 : DB연동

--- spring Project/spring/src/main/java/kr/kh/spring/controller/HomeController.java
+++ spring Project/spring/src/main/java/kr/kh/spring/controller/HomeController.java
@@ -1,18 +1,24 @@
-package kr.kh.spring;
+package kr.kh.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import kr.kh.spring.service.MemberService;
+
 
 @Controller
 public class HomeController {
 	
+	@Autowired
+	MemberService memberService;
+	
 	@RequestMapping(value = "/")
 	public ModelAndView home(ModelAndView mv) {
-		mv.setViewName("home");
+		mv.setViewName("/main/home");
 		return mv;
 	}
 	
@@ -20,28 +26,47 @@ public class HomeController {
 	public ModelAndView ex1(ModelAndView mv,String name, Integer age) {
 		System.out.println("예제1 - 화면에서 전달한 이름 : " + name);
 		System.out.println("예제1 - 화면에서 전달한 나이 : " + age);
-		mv.setViewName("ex1");
+		mv.setViewName("/main/ex1");
 		return mv;
 	}
 	@RequestMapping(value = "/ex2")
 	public ModelAndView ex2(ModelAndView mv,String name, Integer age) {
 		System.out.println("예제2 - 화면에서 전달한 이름 : " + name);
 		System.out.println("예제2 - 화면에서 전달한 나이 : " + age);
-		mv.setViewName("ex2");
+		mv.setViewName("/main/ex2");
 		return mv;
 	}
 	@RequestMapping(value = "/ex3")
 	public ModelAndView ex3(ModelAndView mv) {
-		mv.setViewName("ex3");
+		mv.setViewName("/main/ex3");
 		return mv;
 	}
 	@RequestMapping(value = "/ex3/{name}/{age}")
-	public ModelAndView ex3NameAge3(ModelAndView mv,
+	public ModelAndView exNameAge3(ModelAndView mv,
 			@PathVariable("name")String name,
 			@PathVariable("age")int age) {
 		System.out.println("예제3 - 화면에서 전달한 이름 : " + name);
 		System.out.println("예제3 - 화면에서 전달한 나이 : " + age);
-		mv.setViewName("ex3");
+		mv.setViewName("/main/ex3");
+		return mv;
+	}
+	@RequestMapping(value = "/ex4")
+	public ModelAndView ex4(ModelAndView mv) {
+		/* 서버에서 화면으로 이름과 나이를 전송
+		 * - 화면에서 호출할 이름(변수명)과 값을 지정
+		 * - addObject메소드를 통해서
+		 * */
+		mv.addObject("name","둘리");
+		mv.addObject("age","10000");
+		mv.setViewName("/main/ex4");
+		return mv;
+	}
+	@RequestMapping(value = "/ex5")
+	public ModelAndView ex5(ModelAndView mv, String num) {
+		String name = memberService.getNameByNum(num);
+		mv.addObject("name",name);
+		mv.addObject("num",num);
+		mv.setViewName("/main/ex5");
 		return mv;
 	}
 }
