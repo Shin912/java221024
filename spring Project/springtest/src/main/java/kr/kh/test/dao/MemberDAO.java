fbe562036be3e53663ef43c6de54c2fce9d1eda7
shin129 <lss1994@naver.com>
2023-02-20 19:52:20 +0900

    스프링 9일차 : 회원가입 구현 / 비밀번호 암호화 구현

--- spring Project/springtest/src/main/java/kr/kh/test/dao/MemberDAO.java
+++ spring Project/springtest/src/main/java/kr/kh/test/dao/MemberDAO.java
@@ -2,7 +2,10 @@ package kr.kh.test.dao;
 
 import org.apache.ibatis.annotations.Param;
 
+import kr.kh.test.vo.MemberVO;
+
 public interface MemberDAO {
+
+	int insertMember(@Param("member")MemberVO member);
 	
-	String selectMemberName(@Param("num")Integer num);
 }
