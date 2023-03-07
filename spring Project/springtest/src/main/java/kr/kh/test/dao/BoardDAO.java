94ac7771aafcd1ec5ea480f1941afd961f7ea6ec
shin129 <lss1994@naver.com>
2023-03-07 21:54:50 +0900

    스프링 18일차 : 미완성

--- spring Project/springtest/src/main/java/kr/kh/test/dao/BoardDAO.java
+++ spring Project/springtest/src/main/java/kr/kh/test/dao/BoardDAO.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import org.apache.ibatis.annotations.Param;
 
 import kr.kh.test.vo.BoardTypeVO;
+import kr.kh.test.vo.BoardVO;
+import kr.kh.test.vo.MemberVO;
 
 public interface BoardDAO {
 
@@ -17,5 +19,9 @@ public interface BoardDAO {
 	boolean updateBoardType(@Param("bt")BoardTypeVO bt);
 
 	boolean deleteBoardType(@Param("bt_num")int bt_num);
+
+	ArrayList<BoardTypeVO> selectBoardTypeByName(@Param("me")MemberVO user);
+
+	void insertBoard(@Param("bo")BoardVO board);
 
 }
